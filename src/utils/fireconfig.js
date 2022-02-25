5e357f60480fd83d6597770aab994891cc811314
Ashish <68690233+asheeeshh@users.noreply.github.com>
2022-02-25 21:18:49 +0530

    config vars

---
+++ src/utils/fireconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: process.env["REACT_APP_FR_KEY"],
+	databaseURL: process.env["REACT_APP_FR_URL"],
+	authDomain: process.env["REACT_APP_FR_DOMAIN"],
+	projectId: process.env["REACT_APP_FR_ID"],
+	storageBucket: process.env["REACT_APP_FR_BUCKET"],
+	messagingSenderId: process.env["REACT_APP_FR_MSG_ID"],
+	appId: process.env["REACT_APP_FR_APP_ID"]
+}
