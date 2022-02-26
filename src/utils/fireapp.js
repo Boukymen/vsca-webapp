ff22894adfc035264ba8653e5b4cc681dbc2e139
Ashish <68690233+asheeeshh@users.noreply.github.com>
2022-02-26 11:05:45 +0530

    ......

--- src/utils/fireapp.js
+++ src/utils/fireapp.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
 
+import.meta.env.SOME_VALUE
+
 const firebaseConfig = {
-	apiKey: process.env["REACT_APP_FR_KEY"],
-	databaseURL: process.env["REACT_APP_FR_URL"],
-	authDomain: process.env["REACT_APP_FR_DOMAIN"],
-	projectId: process.env["REACT_APP_FR_ID"],
-	storageBucket: process.env["REACT_APP_FR_BUCKET"],
-	messagingSenderId: process.env["REACT_APP_FR_MSG_ID"],
-	appId: process.env["REACT_APP_FR_APP_ID"]
+	apiKey: import.meta.env.REACT_APP_FR_KEY,
+	databaseURL: import.meta.env.REACT_APP_FR_URL,
+	authDomain: import.meta.env.REACT_APP_FR_DOMAIN,
+	projectId: import.meta.env.REACT_APP_FR_ID,
+	storageBucket: import.meta.env.REACT_APP_FR_BUCKET,
+	messagingSenderId: import.meta.env.REACT_APP_FR_MSG_ID,
+	appId: import.meta.env.REACT_APP_FR_APP_ID
 }
 
 console.log(firebaseConfig)
